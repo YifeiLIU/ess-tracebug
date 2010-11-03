5d27574b146287f01afc6c1b688557efa4b4bdb7
spinuvit <spinuvit@10fa4c3f-5b4b-6f40-6e6a-a60473f2d179>
2010-11-03 22:23:03 +0000

    Created the project

---
+++ trunk/error.data.R
@@ -0,0 +1,37 @@
+### Test errors and patterns the regexps are build for
+
+sdfsdf#sdfsf 4343 
+8: as.array(rv1) at master_pbm.R#58
+sdfsf 
+7: eval(expr, envir, enclos) at pbm_funcs.R#337
+6: eval(ue_chebuild.rv_ll_st.dim) at pbm_funcs.R#337
+5: eval(expr, envir, enclos) at pbm_funcs.R#337
+4: eval(ue_BUILD) at pbm_funcs.R#337
+3: eval(substitute(expr), envir, enclos) at pbm_funcs.R#336
+2: evalq({
+eval(ue_BUILD)
+eval(ue_VALIDATE)
+}, envir = bcell) at pbm_funcs.R#336
+1: bcell(name = "DATA", prototype = .M$dc, LUR = .p(parent = LUR, 
+ix = ixDM, ix_dim = 1), beta = .p(parent = beta, ix = rep.int(1L, 
+length(ixDM)), ix_dim = 1L), st = as.matrix(data$dD[, 1:2]), 
+ll = rep.int(-Inf, length(ixDM)), rv = c(1, 1))
+0:
+
+
+Error in source(file = "/home/vitoshka/Dropbox/works/pbm/master_pbm.R") : 
+ /home/vitoshka/Dropbox/works/pbm/master_pbm.R:494:15: unexpected symbol
+
+Error in source(file = "/home/vitoshka/Dropbox/works/pbm/master_pbm.R") : /home/vitoshka/Dropbox/works/pbm/master_pbm.R:494:15: unexpected symbol
+
+
+# RECOVER:
+
+1: BC(name = "DATA", prototype = .M$dc, LUR = .p(parent = LUR, ix = ixDM, ix_dim = 1), beta = .p(parent = beta, ix = rep.int(1, length(ixDM)), ix_dim = 1)
+2: pbm_funcs.R#403: evalq(.e(q0_INIT.C), envir = bcell)
+3: pbm_funcs.R#403: eval(substitute(expr), envir, enclos)
+4: pbm_funcs.R#403: .e(q0_INIT.C)
+5: pbm_funcs.R#3: eval(expr, envir, envir)
+
+
+          ra
---
+++ trunk/ess-tracebug.R
@@ -0,0 +1,18 @@
+## R code used in the project
+
+
+### DEBUG/UNDEBUG AT POINT
+getDebugged <- function(env){
+    env <- as.environment(env)
+    allF <- ls(envir = env, all.names = T)
+    isDeb <- unlist(lapply(allF,
+                           function(obj) is.function(f <- get(obj, envir = env)) && isdebugged(f)
+                           ))
+    allF[isDeb]
+}
+
+getGenerics()
+str(get("plot"))
+
+methods:::.showMethodsTable
+is(m, "MethodDefinition")
