9be84da69bbe34d3f1c96f6193a8631629e30cc3
spinuvit <spinuvit@10fa4c3f-5b4b-6f40-6e6a-a60473f2d179>
2011-02-09 12:47:06 +0000

    1) Added conditional breakpoints (+ face and spec) 2) Added loggers (+ face and spec) 3) Modified ess-watch-refresh-buffer-visibly to handle no-prompt-check 4) slight modification of the filter function; not it does not wait for the final prompt after exiting the debugger.

--- ess-tracebug.R
+++ ess-tracebug.R
@@ -76,6 +76,8 @@ local({
 
 
 
+
+
 setGeneric("blabla", function(a=34,b=343) standardGeneric("blabla"))
 
 methods:::.showMethodsTable()
@@ -90,253 +92,49 @@ is(m, "MethodDefinition")
     parse(text = "sdfsf + 33"),
     parse(text = "str(iris)"))
 
-assign(".ess_watch_eval", function(){
-    if(!exists(".ess_watch_expressions")){
-        assign(".ess_watch_expressions", list(), envir = .GlobalEnv)
+assign('.ess_watch_eval', function(){
+    if(!exists('.ess_watch_expressions')){
+        assign('.ess_watch_expressions', list(), envir = .GlobalEnv)
     }
     if(length(.ess_watch_expressions) == 0L){
-        cat("\n# Watch list is empty!\n
-# a/i     append/insert new expression
-# k       kill
-# e       edit the expression
-# r       rename
-# n/p     navigate
-# u/U     move the expression up/down
-# q       kill the buffer
-")
+        cat('\n# Watch list is empty!\n
+                                        # a/i     append/insert new expression
+                                        # k       kill
+                                        # e       edit the expression
+                                        # r       rename
+                                        # n/p     navigate
+                                        # u/U     move the expression up/down
+                                        # q       kill the buffer
+            ')
     }else{
+        .parent_frame <- parent.frame()
         .essWEnames <- allNames(.ess_watch_expressions)
         len0p <- !nzchar(.essWEnames)
         .essWEnames[len0p] <- seq_along(len0p)[len0p]
         for(i in seq_along(.ess_watch_expressions)){
-            cat("\n@---- ", .essWEnames[[i]], " ", rep.int("-", max(0, 30 - nchar(.essWEnames[[i]]))), "@\n", sep = "")
-            cat( paste("@--->", deparse(.ess_watch_expressions[[i]][[1L]])), " \n", sep = "")
-            tryCatch(print(eval(.ess_watch_expressions[[i]])),
-                     error = function(e) cat("Error:", e$message, "\n" ),
-                     warning = function(w) cat("warning: ", w$message, "\n"))
+            cat('\n@---- ', .essWEnames[[i]], ' ', rep.int('-', max(0, 35 - nchar(.essWEnames[[i]]))), '->\n', sep = '')
+            cat( paste('@--->', deparse(.ess_watch_expressions[[i]][[1L]])), ' \n', sep = '')
+            tryCatch(print(eval(.ess_watch_expressions[[i]], envir = .parent_frame)),
+                     error = function(e) cat('Error:', e$message, '\n' ),
+                     warning = function(w) cat('warning: ', w$message, '\n' ))
         }}
-}, envir = .GlobalEnv)
+}, envir = .BaseNamespaceEnv); environment(.ess_watch_eval)<-.GlobalEnv
+assign('.ess_log_eval',  function(log_name){
+    if(!exists(log_name, envir = .GlobalEnv, inherits = FALSE))
+        assign(log_name, list(), envir = .GlobalEnv)
+    log <- get(log_name, envir = .GlobalEnv, inherits = FALSE)
+    .essWEnames <- allNames(.ess_watch_expressions)
+    cur_log <- list()
+    .parent_frame <- parent.frame()
+    for(i in seq_along(.ess_watch_expressions))
+        cur_log[[i]] <-
+            tryCatch(eval(.ess_watch_expressions[[i]]), envir = .parent_frame,
+                     error = function(e) paste('Error:', e$message, '\n'),
+                     warning = function(w) paste('warning: ', w$message, '\n'))
+    names(cur_log) <- .essWEnames
+    assign(log_name, c(log, list(cur_log)), envir = .GlobalEnv)
+    invisible(NULL)
+}, envir = .BaseNamespaceEnv); environment(.ess_log_eval) <- .GlobalEnv
 
 
 
-function (what, tracer = NULL, exit = NULL, at = numeric(), print = TRUE,
-          signature = NULL, where = .GlobalEnv, edit = FALSE, from = NULL,
-          untrace = FALSE)
-{
-    if (is.function(where)) {
-        if (is(where, "genericFunction"))
-            where <- parent.env(environment(where))
-        else where <- environment(where)
-        fromPackage <- getPackageName(where)
-    }
-    else fromPackage <- ""
-    doEdit <- !identical(edit, FALSE)
-    whereF <- NULL
-    pname <- character()
-    def <- NULL
-    if (is.function(what)) {
-        def <- what
-        if (is(def, "genericFunction")) {
-            what <- def@generic
-            whereF <- .genEnv(what, where)
-            pname <- def@package
-        }
-        else {
-            fname <- substitute(what)
-            if (is.name(fname)) {
-                what <- as.character(fname)
-                temp <- .findFunEnvAndName(what, where)
-                whereF <- temp$whereF
-                pname <- temp$pname
-            }
-            else if (is.call(fname) && identical(fname[[1L]],
-                                                 as.name("::"))) {
-                whereF <- as.character(fname[[2L]])
-                require(whereF, character.only = TRUE)
-                whereF <- as.environment(paste("package", whereF,
-                                               sep = ":"))
-                pname <- fname[[2L]]
-                what <- as.character(fname[[3L]])
-            }
-            else if (is.call(fname) && identical(fname[[1L]],
-                                                 as.name(":::"))) {
-                pname <- paste(fname[[2L]], "(not-exported)")
-                whereF <- loadNamespace(as.character(fname[[2L]]))
-                what <- as.character(fname[[3L]])
-            }
-            else stop("argument 'what' should be the name of a function")
-        }
-    }
-    else {
-        what <- as(what, "character")
-        if (length(what) != 1) {
-            for (f in what) {
-                if (nargs() == 1)
-                    trace(f)
-                else Recall(f, tracer, exit, at, print, signature,
-                            where, edit, from, untrace)
-            }
-            return(what)
-        }
-        temp <- .findFunEnvAndName(what, where, signature)
-        whereF <- temp$whereF
-        pname <- temp$pname
-    }
-    if (what %in% .InvalidTracedFunctions)
-        stop(gettextf("Tracing the internal function \"%s\" is not allowed",
-                      what))
-    if (.traceTraceState) {
-        message(".TraceWithMethods: after computing what, whereF")
-        browser()
-    }
-    if (nargs() == 1)
-        return(.primTrace(what))
-    if (is.null(whereF)) {
-        allWhere <- findFunction(what, where = where)
-        if (length(allWhere) == 0)
-            stop(gettextf("no function definition for \"%s\" found",
-                          what), domain = NA)
-        whereF <- as.environment(allWhere[[1L]])
-    }
-    if (is.null(tracer) && is.null(exit) && identical(edit, FALSE))
-        tracer <- quote({
-        })
-    if (is.null(def))
-        def <- getFunction(what, where = whereF)
-    if (is(def, "traceable") && identical(edit, FALSE) && !untrace)
-        def <- .untracedFunction(def)
-    if (!is.null(signature)) {
-        fdef <- if (is.primitive(def))
-            getGeneric(what, TRUE, where)
-        else def
-        def <- selectMethod(what, signature, fdef = fdef, optional = TRUE)
-        if (is.null(def)) {
-            warning(gettextf("Can't untrace method for \"%s\"; no method defined for this signature: %s",
-                             what, paste(signature, collapse = ", ")))
-            return(def)
-        }
-    }
-    if (untrace) {
-        if (.traceTraceState) {
-            message(".TraceWithMethods: untrace case")
-            browser()
-        }
-        if (is.null(signature)) {
-            if (is(def, "traceable")) {
-                newFun <- .untracedFunction(def)
-            }
-            else {
-                .primUntrace(what)
-                return(what)
-            }
-        }
-        else {
-            if (is(def, "traceable"))
-                newFun <- .untracedFunction(def)
-            else {
-                warning(gettextf("the method for \"%s\" for this signature was not being traced",
-                                 what), domain = NA)
-                return(what)
-            }
-        }
-    }
-    else {
-        if (!is.null(exit)) {
-            if (is.function(exit)) {
-                tname <- substitute(exit)
-                if (is.name(tname))
-                    exit <- tname
-                exit <- substitute(TRACE(), list(TRACE = exit))
-            }
-        }
-        if (!is.null(tracer)) {
-            if (is.function(tracer)) {
-                tname <- substitute(tracer)
-                if (is.name(tname))
-                    tracer <- tname
-                tracer <- substitute(TRACE(), list(TRACE = tracer))
-            }
-        }
-        original <- .untracedFunction(def)
-        traceClass <- .traceClassName(class(original))
-        if (is.null(getClassDef(traceClass)))
-            traceClass <- .makeTraceClass(traceClass, class(original))
-        if (doEdit && is.environment(edit)) {
-            def <- .findNewDefForTrace(what, signature, edit,
-                                       fromPackage)
-            environment(def) <- environment(original)
-            if (is.null(c(tracer, exit))) {
-                newFun <- new(traceClass, original)
-                newFun@.Data <- def
-            }
-            else {
-                newFun <- new(traceClass, def = def, tracer = tracer,
-                              exit = exit, at = at, print = print, doEdit = FALSE)
-                newFun@original <- original
-            }
-            newFun@source <- edit
-        }
-        else newFun <- new(traceClass, def = if (doEdit)
-                           def
-        else original, tracer = tracer, exit = exit, at = at,
-                           print = print, doEdit = edit)
-    }
-    global <- identical(whereF, .GlobalEnv)
-    if (.traceTraceState) {
-        message(".TraceWithMethods: about to assign or setMethod")
-        browser()
-    }
-    if (is.null(signature)) {
-        if (bindingIsLocked(what, whereF))
-            .assignOverBinding(what, newFun, whereF, global)
-        else assign(what, newFun, whereF)
-        if (length(grep("[^.]+[.][^.]+", what)) > 0) {
-            S3MTableName <- ".__S3MethodsTable__."
-            tracedFun <- get(what, envir = whereF, inherits = TRUE)
-            if (exists(S3MTableName, envir = whereF, inherits = FALSE)) {
-                tbl <- get(S3MTableName, envir = whereF, inherits = FALSE)
-                if (exists(what, envir = tbl, inherits = FALSE))
-                    assign(what, tracedFun, envir = tbl)
-            }
-        }
-    }
-    else {
-        if (untrace && is(newFun, "MethodDefinition") && !identical(newFun@target,
-                                                                    newFun@defined))
-            newFun <- NULL
-        setMethod(fdef, signature, newFun, where = baseenv())
-    }
-    if (!global) {
-        action <- if (untrace)
-            "Untracing"
-        else "Tracing"
-        nameSpaceCase <- FALSE
-        location <- if (.identC(fromPackage, "")) {
-            if (length(pname) == 0 && !is.null(whereF))
-                pname <- getPackageName(whereF)
-            nameSpaceCase <- isNamespace(whereF) && !is.na(match(pname,
-                                                                 loadedNamespaces())) && identical(whereF, getNamespace(pname))
-            if (length(pname) == 0)
-                "\""
-            else {
-                if (nameSpaceCase)
-                    paste("\" in environment <namespace:", pname,
-                          ">", sep = "")
-                else paste("\" in package \"", pname, "\"", sep = "")
-            }
-        }
-        else paste("\" as seen from package \"", fromPackage,
-                   "\"", sep = "")
-        object <- if (is.null(signature))
-            " function \""
-        else " specified method for function \""
-        .message(action, object, what, location)
-        if (nameSpaceCase && !untrace && exists(what, envir = .GlobalEnv)) {
-            untcall <- paste("untrace(\"", what, "\", where = getNamespace(\"",
-                             pname, "\"))", sep = "")
-            .message("Warning: Tracing only in the namespace; to untrace you will need:\n    ",
-                     untcall, "\n")
-        }
-    }
-    what
-}
